6d3d855938e2feb6ced06a664e209828041a818a
Brian Sunbury <briansunbury@Brians-MacBook-Pro.local>
2022-04-14 11:56:01 -0700

    sprites working

--- README.md
+++ README.md
@@ -4,5 +4,4 @@
 
 1. `git clone https://github.com/srianbury/meat-party`
 2. `yarn` to install node modules
-3. `npm install -g parcel-bundler` to install parcel globally
-4. `yarn start` to start the app
+3. `yarn start` to start the app
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import { initKaboom } from "./kaboom";
 import { mattsPart } from "./matt";
 import { lhitosPart } from "./lhito";
+import player1 from "../public/sprites/player1.png";
+import player2 from "../public/sprites/player2.png";
 
 initKaboom();
 
-scene("main", () => {
-  mattsPart();
-  lhitosPart();
-  loadSprite("apple", "https://kaboomjs.com/sprites/apple.png");
+loadSprite("player1", player1);
+loadSprite("player2", player2);
 
-  add([sprite("apple"), pos(80, 40), area(), body()]);
-});
+add([text("hi lhito"), pos(120, 80)]);
+add([sprite("player1"), pos(120, 80)]);
+add([sprite("player2"), pos(80, 120)]);
 
-start("main");
+mattsPart();
+lhitosPart();
