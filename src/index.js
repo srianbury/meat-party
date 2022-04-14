73e62843467e111239013e30538e7d97d1986999
Brian Sunbury <briansunbury@Brians-MacBook-Pro.local>
2022-04-13 22:16:52 -0700

    load sprite issue

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ initKaboom();
 scene("main", () => {
   mattsPart();
   lhitosPart();
+  loadSprite("apple", "https://kaboomjs.com/sprites/apple.png");
+
+  add([sprite("apple"), pos(80, 40), area(), body()]);
 });
 
 start("main");
