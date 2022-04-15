43656335145741f70bfa529fe5ca90901e999f28
srianbury <srianbury@users.noreply.github.com>
2022-04-15 04:42:13 +0000

    Prettified Code!

--- src/matt.js
+++ src/matt.js
@@ -1,37 +1,25 @@
 function mattsPart() {
-
   //scene("game", ()=> {
-    add([
-        text("NEW GAME!"),
-        pos(1,500),
-        origin("left"),
-        color(255,0,0),
-        layer("UI"),
-        area(),
-        move(RIGHT, 500),
-        cleanup()
-        
-    ])
- // })
+  add([
+    text("NEW GAME!"),
+    pos(1, 500),
+    origin("left"),
+    color(255, 0, 0),
+    layer("UI"),
+    area(),
+    move(RIGHT, 500),
+    cleanup(),
+  ]);
+  // })
 
- const score = add([
-  text(0),
-  pos(12, 12),
-  fixed(),
-])
+  const score = add([text(0), pos(12, 12), fixed()]);
 
-const times = add([
-  text(0),
-  pos(900,5),
-  fixed(),
-  { time: 0 },
-])
+  const times = add([text(0), pos(900, 5), fixed(), { time: 0 }]);
 
-times.onUpdate(() => {
-  times.time += dt()
-  times.text = times.time.toFixed(2)
-})
-
+  times.onUpdate(() => {
+    times.time += dt();
+    times.text = times.time.toFixed(2);
+  });
 }
 
 export { mattsPart };
