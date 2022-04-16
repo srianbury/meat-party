f5a9b5af8a11e7815dea2d7e3aae9fd966763368
Total Motivation <kazfam2@sbcglobal.net>
2022-04-16 15:28:18 -0500

    shelf sprites

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import player1 from "../public/sprites/player1.png";
 import player2 from "../public/sprites/player2.png";
 import oven from "../public/sprites/oven.png";
 import bean from "../public/sprites/bean.png";
+import shelf from "../public/sprites/shelf.png";
 import { startScene } from "./startScene";
 import { fightScene } from "./fightScene";
 import { endScene } from "./endScene";
@@ -14,6 +15,7 @@ loadSprite("player1", player1);
 loadSprite("player2", player2);
 loadSprite("oven", oven);
 loadSprite("bean", bean);
+loadSprite("shelf", shelf);
 
 // add([sprite("player1"), pos(120, 80),area(),body()]);
 // add([sprite("player2"), pos(80, 120)]);
