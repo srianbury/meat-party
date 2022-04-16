dc45046fc3de5babe0c9459c1d7a8cc26b6f83e9
Brian Sunbury <briansunbury@Brians-MacBook-Pro.local>
2022-04-15 22:01:55 -0700

    refactor

--- src/fightScene.js
+++ src/fightScene.js
@@ -1,13 +1,11 @@
 import { lhitosPart } from "./lhito";
 import { mattsPart } from "./matt";
-import { item } from "./item";
 
 function fightScene(initPlayingField) {
   scene("fight", () => {
     initPlayingField();
     mattsPart();
     lhitosPart();
-    item();
   });
 }
 
