9154cae5c02ce4c27405258e4ed901892dee08ac
Lhito <Lhito@users.noreply.github.com>
2022-04-18 20:51:24 +0000

    Prettified Code!

--- src/options.js
+++ src/options.js
@@ -1,19 +1,19 @@
 let playerControls = [
-    [
-        "w",//up
-        "s",//down
-        "a",//left
-        "d",//right
-        "tab",//attack
-    ],
-    [
-        "up",//up
-        "down",//down
-        "left",//left
-        "right",//right
-        "enter",//attack
-    ]
-]
+  [
+    "w", //up
+    "s", //down
+    "a", //left
+    "d", //right
+    "tab", //attack
+  ],
+  [
+    "up", //up
+    "down", //down
+    "left", //left
+    "right", //right
+    "enter", //attack
+  ],
+];
 
 function options() {
   const optionsBarText = ["Controls", "Dummy1", "Dummy2"];
@@ -26,185 +26,181 @@ function options() {
     let selectedPlayer = 0;
 
     //generates a list of buttons for how many players we want in our game
-    playerList.forEach((player,index) => {
-        //for the initial selected opacity generation
-        if(index === 0){
-            buttonOpacity = selectedOpacityValue;
-        }else{
-            buttonOpacity = opacityValue;
-        }
+    playerList.forEach((player, index) => {
+      //for the initial selected opacity generation
+      if (index === 0) {
+        buttonOpacity = selectedOpacityValue;
+      } else {
+        buttonOpacity = opacityValue;
+      }
 
-        add([
-            "playerButton",
-            rect(width() * 0.1, 48),
-            pos(width() * (0.3 * (index + 1)), height() * 0.4),
-            outline(4),
-            opacity(buttonOpacity),
-            area(),
-            origin("center"),
-            color(0, 255, 255),
-            {
-                index: index
-            },
-            text(player),
-          ]);
+      add([
+        "playerButton",
+        rect(width() * 0.1, 48),
+        pos(width() * (0.3 * (index + 1)), height() * 0.4),
+        outline(4),
+        opacity(buttonOpacity),
+        area(),
+        origin("center"),
+        color(0, 255, 255),
+        {
+          index: index,
+        },
+        text(player),
+      ]);
     });
 
     //image for up button
     add([
-        "upButton",
-        rect(width() * 0.1, 48),
-        pos(width() * 0.3, height() * 0.6),
-        outline(4),
-        area(),
-        origin("center"),
-        color(224, 224, 224),
-        text("Up"),
+      "upButton",
+      rect(width() * 0.1, 48),
+      pos(width() * 0.3, height() * 0.6),
+      outline(4),
+      area(),
+      origin("center"),
+      color(224, 224, 224),
+      text("Up"),
     ]);
 
     //value for selected player's up control
     add([
-        "values",
-        rect(width() * 0.1, 48),
-        pos(width() * 0.4, height() * 0.6),
-        outline(4),
-        area(),
-        origin("center"),
-        color(0, 255, 0),
-        {
-            purpose: 0
-        },
-        text(playerControls[selectedPlayer].up),
-    ]); 
+      "values",
+      rect(width() * 0.1, 48),
+      pos(width() * 0.4, height() * 0.6),
+      outline(4),
+      area(),
+      origin("center"),
+      color(0, 255, 0),
+      {
+        purpose: 0,
+      },
+      text(playerControls[selectedPlayer].up),
+    ]);
 
     //image for down button
     add([
-        "downButton",
-        rect(width() * 0.1, 48),
-        pos(width() * 0.3, height() * 0.8),
-        outline(4),
-        area(),
-        origin("center"),
-        color(224, 224, 224),
-        text("Down"),
-      ]);
+      "downButton",
+      rect(width() * 0.1, 48),
+      pos(width() * 0.3, height() * 0.8),
+      outline(4),
+      area(),
+      origin("center"),
+      color(224, 224, 224),
+      text("Down"),
+    ]);
 
     //value for selected player's down control
     add([
-        "values",
-        rect(width() * 0.1, 48),
-        pos(width() * 0.43, height() * 0.8),
-        outline(4),
-        area(),
-        origin("center"),
-        color(0, 255, 0),
-        {
-            purpose: 1
-        },
-        text(playerControls[selectedPlayer].down),
-    ]); 
-
-    
+      "values",
+      rect(width() * 0.1, 48),
+      pos(width() * 0.43, height() * 0.8),
+      outline(4),
+      area(),
+      origin("center"),
+      color(0, 255, 0),
+      {
+        purpose: 1,
+      },
+      text(playerControls[selectedPlayer].down),
+    ]);
 
     //image for right button
     add([
-        "rightButton",
-        rect(width() * 0.1, 48),
-        pos(width() * 0.45, height() * 0.7),
-        outline(4),
-        area(),
-        origin("center"),
-        color(224, 224, 224),
-        text("Right"),
-      ]);
+      "rightButton",
+      rect(width() * 0.1, 48),
+      pos(width() * 0.45, height() * 0.7),
+      outline(4),
+      area(),
+      origin("center"),
+      color(224, 224, 224),
+      text("Right"),
+    ]);
 
     //value for selected player's right control
     add([
-        "values",
-        rect(width() * 0.1, 48),
-        pos(width() * 0.58, height() * 0.7),
-        outline(4),
-        area(),
-        origin("center"),
-        color(0, 255, 0),
-        {
-            purpose: 2
-        },
-        text(playerControls[selectedPlayer].right),
-    ]); 
+      "values",
+      rect(width() * 0.1, 48),
+      pos(width() * 0.58, height() * 0.7),
+      outline(4),
+      area(),
+      origin("center"),
+      color(0, 255, 0),
+      {
+        purpose: 2,
+      },
+      text(playerControls[selectedPlayer].right),
+    ]);
 
     //image for left button
     add([
-        "leftButton",
-        rect(width() * 0.1, 48),
-        pos(width() * 0.16, height() * 0.7),
-        outline(4),
-        area(),
-        origin("center"),
-        color(224, 224, 224),
-        text("Left"),
-      ]);
+      "leftButton",
+      rect(width() * 0.1, 48),
+      pos(width() * 0.16, height() * 0.7),
+      outline(4),
+      area(),
+      origin("center"),
+      color(224, 224, 224),
+      text("Left"),
+    ]);
 
     //value for selected player's left control
     add([
-        "values",
-        rect(width() * 0.1, 48),
-        pos(width() * 0.3, height() * 0.7),
-        outline(4),
-        area(),
-        origin("center"),
-        color(0, 255, 0),
-        {
-            purpose: 3
-        },
-        text(playerControls[selectedPlayer].left),
-    ]); 
+      "values",
+      rect(width() * 0.1, 48),
+      pos(width() * 0.3, height() * 0.7),
+      outline(4),
+      area(),
+      origin("center"),
+      color(0, 255, 0),
+      {
+        purpose: 3,
+      },
+      text(playerControls[selectedPlayer].left),
+    ]);
 
     //image for attack button
     add([
-        "attackButton",
-        rect(width() * 0.1, 48),
-        pos(width() * 0.75, height() * 0.7),
-        outline(4),
-        area(),
-        origin("center"),
-        color(224, 224, 224),
-        text("Attack"),
-      ]);
+      "attackButton",
+      rect(width() * 0.1, 48),
+      pos(width() * 0.75, height() * 0.7),
+      outline(4),
+      area(),
+      origin("center"),
+      color(224, 224, 224),
+      text("Attack"),
+    ]);
 
     //value for selected player's attack control
     add([
-        "values",
-        rect(width() * 0.1, 48),
-        pos(width() * 0.9, height() * 0.7),
-        outline(4),
-        area(),
-        origin("center"),
-        color(0, 255, 0),
-        {
-            purpose: 4
-        },
-        text(playerControls[selectedPlayer].attack),
-    ]); 
+      "values",
+      rect(width() * 0.1, 48),
+      pos(width() * 0.9, height() * 0.7),
+      outline(4),
+      area(),
+      origin("center"),
+      color(0, 255, 0),
+      {
+        purpose: 4,
+      },
+      text(playerControls[selectedPlayer].attack),
+    ]);
 
-      //when selected player updates
-      onUpdate("values", (control) => {
-        control.text = playerControls[selectedPlayer][control.purpose];
-      })
+    //when selected player updates
+    onUpdate("values", (control) => {
+      control.text = playerControls[selectedPlayer][control.purpose];
+    });
 
-      //onclick for player buttons
-      onClick("playerButton", (button) => {
-        button.opacity = 1.0;
+    //onclick for player buttons
+    onClick("playerButton", (button) => {
+      button.opacity = 1.0;
 
-        if(selectedPlayer !== button.index){
-            let previousSelectedButton = get("playerButton")[selectedPlayer];
-            previousSelectedButton.opacity = 0.5;
-            selectedPlayer = button.index;
-        }
-
-
-      });
-  };
+      if (selectedPlayer !== button.index) {
+        let previousSelectedButton = get("playerButton")[selectedPlayer];
+        previousSelectedButton.opacity = 0.5;
+        selectedPlayer = button.index;
+      }
+    });
+  }
 
   scene("options", () => {
     add([
@@ -244,14 +240,14 @@ function options() {
 
     //general box for options
     add([
-        "optionBox",
-        rect(width() * 0.95, height() * 0.6),
-        pos(width() * 0.5, height() * 0.6),
-        outline(4),
-        area(),
-        color(128, 128, 128),
-        origin("center"),
-      ]);
+      "optionBox",
+      rect(width() * 0.95, height() * 0.6),
+      pos(width() * 0.5, height() * 0.6),
+      outline(4),
+      area(),
+      color(128, 128, 128),
+      origin("center"),
+    ]);
 
     //can look into procedurally generating onclicks for the options bar and maybe store all of the functions and adds that will display in another array of objects
     onClick(optionsBarText[0], () => controlsOptions());
