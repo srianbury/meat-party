0d18987df8c0d5c0770d1176f4016a664913672b
Total Motivation <kazfam2@sbcglobal.net>
2022-04-19 10:20:06 -0500

    added sprites

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import pepper from "../public/sprites/pepper.png";
 import shelf from "../public/sprites/shelf2.png";
 import banana from "../public/sprites/banana.png";
 import reloaded from "url:../public/sounds/reloaded.mp3";
+import carrot from "../public/sprites/Carrot.png";
+import Chicken from "../public/sprites/Chicken.png";
+import Chopper from "../public/sprites/Chopper.png";
+import Pan from "../public/sprites/Frying Pan.png";
+import Salt from "../public/sprites/Salt.png";
+import Steak from "../public/sprites/Steak.png";
+
 import { startScene } from "./startScene";
 import { fightScene } from "./fightScene";
 import { endScene } from "./endScene";
@@ -21,6 +28,12 @@ loadSprite("bean", bean);
 loadSprite("pepper", pepper);
 loadSprite("shelf", shelf);
 loadSprite("banana", banana);
+loadSprite("carrot", carrot);
+loadSprite("chicken", Chicken);
+loadSprite("chopper", Chopper);
+loadSprite("pan", Pan);
+loadSprite("salt", Salt);
+loadSprite("steak", Steak);
 loadSound("reloaded", reloaded);
 
 startScene(initPlayingField);
